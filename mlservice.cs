7c98829ed44332eb84c001daeb839c07de65f842
DiegoFjord <138102450+DiegoFjord@users.noreply.github.com>
2024-02-05 20:50:01 -0800

    Update mlservice.cs styles

--- mlservice.cs
+++ mlservice.cs
@@ -1,10 +1,7 @@
 ﻿using ConsoleApp2;
-using Microsoft.AspNetCore.Hosting;
-using System;
 using System.Diagnostics;
-using System.Net;
 
-namespace redone.Services
+namespace myapp.Services
 {
     public class mlservice
     {
@@ -12,35 +9,7 @@ namespace redone.Services
         {
             WebHostEnvironment = webHostEnvironment;
         }
-
         public IWebHostEnvironment WebHostEnvironment { get; }
-
-        public string Show()
-        {
-            var sampleData = new MLModel1.ModelInput()
-            {
-                Datetime = @"2021-02-26",
-                Temp = 30F,
-                Dew = 16.2F,
-                Humidity = 58.6F,
-                Precip = 0F,
-                Precipprob = 0F,
-                Precipcover = 0F,
-                Windgust = 44F,
-                Windspeed = 36.4F,
-                Cloudcover = 45.6F,
-                Visibility = 12F,
-                Solarradiation = 231.8F,
-                Uvindex = 9F,
-                Sunrise = @"2021-02-26 6:53:52",
-                Sunset = @"2021-02-26 18:34:50",
-            };
-            var result = MLModel1.Predict(sampleData);
-            Debug.WriteLine(result.Score);
-            string show = result.Score.ToString();
-            return (show);
-        }
-
         public string createprediction( float Temp,
                                         float Dew,
                                         float Humidity,
